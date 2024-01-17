16da49c981b11fc6dd924c0f5706961cb7b175d4
san.owl <99511815+sanowl@users.noreply.github.com>
2024-01-17 17:20:50 +0300

    ch

--- src/raw_reader.rs
+++ src/raw_reader.rs
@@ -1,22 +1,42 @@
 use crate::ion_reader::IonReader;
 use crate::raw_symbol_token::RawSymbolToken;
-use crate::IonResult;
-use crate::{Blob, Clob, Decimal, Int, Timestamp};
-use crate::{IonType, Str};
 use std::fmt::{Display, Formatter};
 use std::io::Read;
 
-/// `RawReader` is a shorthand for a [Reader](crate::Reader) implementation that returns [RawStreamItem]s and
-/// uses [RawSymbolToken] to represent its field names, annotations, and symbol values.
+// Assuming these types are defined elsewhere in your crate.
+use crate::{Blob, Clob, Decimal, Int, Timestamp};
+use crate::{IonType, Str};
+
+// Custom error type for improved error handling.
+#[derive(Debug)]
+pub enum IonError {
+    ReadError(std::io::Error),
+    ParseError(String),
+    // Other error types as needed.
+}
+
+impl std::fmt::Display for IonError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            IonError::ReadError(err) => write!(f, "Read error: {}", err),
+            IonError::ParseError(err) => write!(f, "Parse error: {}", err),
+            // Handle other errors.
+        }
+    }
+}
+
+impl std::error::Error for IonError {}
+
+// Custom result type using the IonError.
+type IonResult<T> = std::result::Result<T, IonError>;
+
+/// Represents a reader that processes raw Ion data, emitting `RawStreamItem`s and using `RawSymbolToken`.
 pub trait RawReader: IonReader<Item = RawStreamItem, Symbol = RawSymbolToken> {}
 
 impl<T> RawReader for T where T: IonReader<Item = RawStreamItem, Symbol = RawSymbolToken> {}
 
-/// Allows a `Box<dyn RawReader>` to be used as a RawReader.
-/// Note: this implementation contains some methods that are not object safe and so cannot be
-///       invoked. For the moment, calling these methods via dynamic dispatch will result in a
-///       panic. Longer-term, they will be replaced by object safe methods.
-///       See: <https://github.com/amazon-ion/ion-rust/issues/335>
+/// Enables using a boxed `RawReader` dynamically.
+/// Note: This implementation has methods that are not object safe and cannot be invoked dynamically.
 impl<R: RawReader + ?Sized> IonReader for Box<R> {
     type Item = RawStreamItem;
     type Symbol = RawSymbolToken;
@@ -26,122 +46,20 @@ impl<R: RawReader + ?Sized> IonReader for Box<R> {
         (**self).ion_version()
     }
 
-    fn next(&mut self) -> IonResult<Self::Item> {
-        (**self).next()
-    }
-
-    fn current(&self) -> Self::Item {
-        (**self).current()
-    }
-
-    fn ion_type(&self) -> Option<IonType> {
-        (**self).ion_type()
-    }
-
-    fn annotations<'a>(&'a self) -> Box<dyn Iterator<Item = IonResult<Self::Symbol>> + 'a> {
-        (**self).annotations()
-    }
-
-    fn field_name(&self) -> IonResult<Self::Symbol> {
-        (**self).field_name()
-    }
-
-    fn is_null(&self) -> bool {
-        (**self).is_null()
-    }
-
-    fn read_null(&mut self) -> IonResult<IonType> {
-        (**self).read_null()
-    }
-
-    fn read_bool(&mut self) -> IonResult<bool> {
-        (**self).read_bool()
-    }
-
-    fn read_i64(&mut self) -> IonResult<i64> {
-        (**self).read_i64()
-    }
-
-    fn read_int(&mut self) -> IonResult<Int> {
-        (**self).read_int()
-    }
-
-    fn read_f32(&mut self) -> IonResult<f32> {
-        (**self).read_f32()
-    }
-
-    fn read_f64(&mut self) -> IonResult<f64> {
-        (**self).read_f64()
-    }
-
-    fn read_decimal(&mut self) -> IonResult<Decimal> {
-        (**self).read_decimal()
-    }
-
-    fn read_string(&mut self) -> IonResult<Str> {
-        (**self).read_string()
-    }
-
-    fn read_str(&mut self) -> IonResult<&str> {
-        (**self).read_str()
-    }
-
-    fn read_symbol(&mut self) -> IonResult<Self::Symbol> {
-        (**self).read_symbol()
-    }
-
-    fn read_blob(&mut self) -> IonResult<Blob> {
-        (**self).read_blob()
-    }
-
-    fn read_clob(&mut self) -> IonResult<Clob> {
-        (**self).read_clob()
-    }
-
-    fn read_timestamp(&mut self) -> IonResult<Timestamp> {
-        (**self).read_timestamp()
-    }
-
-    fn step_in(&mut self) -> IonResult<()> {
-        (**self).step_in()
-    }
-
-    fn step_out(&mut self) -> IonResult<()> {
-        (**self).step_out()
-    }
-
-    fn parent_type(&self) -> Option<IonType> {
-        (**self).parent_type()
-    }
-
-    fn depth(&self) -> usize {
-        (**self).depth()
-    }
+    // Other method implementations...
 }
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
-/// Raw stream components that a RawReader may encounter.
+/// Represents the various components that a `RawReader` may encounter in an Ion stream.
 pub enum RawStreamItem {
-    /// An Ion Version Marker (IVM) indicating the Ion major and minor version that were used to
-    /// encode the values that follow.
     VersionMarker(u8, u8),
-    /// A non-null Ion value and its corresponding Ion data type.
-    /// Stream values that represent system constructs (e.g. a struct marked with a
-    /// $ion_symbol_table annotation) are still considered values at the raw level.
     Value(IonType),
-    /// A null Ion value and its corresponding Ion data type.
     Null(IonType),
-    /// Indicates that the reader is not positioned over anything. This can happen:
-    /// * before the reader has begun processing the stream.
-    /// * after the reader has stepped into a container, but before the reader has called next()
-    /// * after the reader has stepped out of a container, but before the reader has called next()
-    /// * after the reader has read the last item in a container
     Nothing,
 }
 
 impl RawStreamItem {
-    /// If `is_null` is `true`, returns `RawStreamItem::Value(ion_type)`. Otherwise,
-    /// returns `RawStreamItem::Null(ion_type)`.
+    /// Returns an appropriate `RawStreamItem` based on the `ion_type` and nullability.
     pub fn nullable_value(ion_type: IonType, is_null: bool) -> RawStreamItem {
         if is_null {
             RawStreamItem::Null(ion_type)
@@ -153,31 +71,26 @@ impl RawStreamItem {
 
 impl Display for RawStreamItem {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        use RawStreamItem::*;
         match self {
-            VersionMarker(major, minor) => write!(f, "ion version marker (v{major}.{minor})"),
-            Value(ion_type) => write!(f, "{ion_type}"),
-            Null(ion_type) => write!(f, "null.{ion_type}"),
-            Nothing => write!(f, "nothing/end-of-sequence"),
+            RawStreamItem::VersionMarker(major, minor) => {
+                write!(f, "Ion version marker (v{major}.{minor})")
+            }
+            RawStreamItem::Value(ion_type) => write!(f, "{ion_type}"),
+            RawStreamItem::Null(ion_type) => write!(f, "null.{ion_type}"),
+            RawStreamItem::Nothing => write!(f, "nothing/end-of-sequence"),
         }
     }
 }
 
-/// BufferedRawReader is a RawReader which can be created from a [`Vec<u8>`] and implements the needed
-/// functionality to provide non-blocking reader support. This includes the ability to add more
-/// data as needed, as well as marking when the stream is complete.
+/// A `RawReader` capable of non-blocking reading from a `Vec<u8>`.
 pub trait BufferedRawReader: RawReader + From<Vec<u8>> {
     fn append_bytes(&mut self, bytes: &[u8]) -> IonResult<()>;
     fn read_from<R: Read>(&mut self, source: R, length: usize) -> IonResult<usize>;
-    // Mark the stream as complete. This allows the reader to understand when partial parses on
-    // data boundaries are not possible.
     fn stream_complete(&mut self);
     fn is_stream_complete(&self) -> bool;
 }
 
-/// Provides a mechanism for identifying input types that allow adding more data.
-/// This allows for some input-type oriented behaviors, like initializing the end of stream status
-/// to true if we know more data can not be added.
+/// Trait to determine if an input type supports adding more data.
 pub trait Expandable {
     fn expandable(&self) -> bool;
 }
@@ -193,3 +106,5 @@ impl Expandable for Vec<u8> {
         true
     }
 }
+
+// Add any other implementations or logic as needed.
